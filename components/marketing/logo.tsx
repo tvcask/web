5eb21b87e196c018f79e1e9daa88c26427744b33
lucasnevespereira <pereiraneveslucas@gmail.com>
2026-07-07 15:08:05 +0200

    uppercas title

--- components/marketing/logo.tsx
+++ components/marketing/logo.tsx
@@ -3,8 +3,14 @@ import { site } from "@/lib/site";
 export function Logo() {
   return (
     <div className="flex items-center gap-2.5">
-      <img src={site.logo} alt="" className="size-[30px] rounded-[8px] object-cover" />
-      <span className="display whitespace-nowrap text-[17px] text-white">{site.name}</span>
+      <img
+        src={site.logo}
+        alt=""
+        className="size-[30px] rounded-[8px] object-cover"
+      />
+      <span className="display whitespace-nowrap text-[17px] text-white uppercase">
+        {site.name}
+      </span>
     </div>
   );
 }
