550efc1f335d8856b04754cf6c61e660be80f0ce
lucasnevespereira <pereiraneveslucas@gmail.com>
2026-07-10 00:36:30 +0200

    fix(brand): trim logo padding and size header logo to match wordmark

--- components/marketing/logo.tsx
+++ components/marketing/logo.tsx
@@ -3,16 +3,9 @@ import { site } from "@/lib/site";
 export function Logo() {
   return (
     <div className="flex items-center gap-2.5">
-      {/*<img
-        src="/logo-vertical.png"
-        alt=""
-        className="size-[28px] object-contain"
-      />*/}
-      <img
-        src="/logo-horizontal.png"
-        alt=""
-        className="h-[28px] w-auto object-contain"
-      />
+      {/* Switch the two srcs to swap logo variant. Both use height-based sizing
+          so neither gets squished. Vertical alt: h-[32px]. */}
+      <img src="/logo-horizontal.png" alt="" className="h-[26px] w-auto object-contain" />
       <span className="display whitespace-nowrap text-[17px] lowercase text-white">
         {site.displayName}
       </span>
