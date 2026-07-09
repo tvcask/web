e9ef197bdd03b8c3e81ceaa6ce1eeb020012b56f
lucasnevespereira <pereiraneveslucas@gmail.com>
2026-07-10 00:30:44 +0200

    feat(brand): use real horizontal render and switch header to horizontal logo

--- components/marketing/logo.tsx
+++ components/marketing/logo.tsx
@@ -3,8 +3,19 @@ import { site } from "@/lib/site";
 export function Logo() {
   return (
     <div className="flex items-center gap-2.5">
-      <img src="/logo-vertical.png" alt="" className="size-[28px] object-contain" />
-      <span className="display whitespace-nowrap text-[17px] lowercase text-white">{site.displayName}</span>
+      {/*<img
+        src="/logo-vertical.png"
+        alt=""
+        className="size-[28px] object-contain"
+      />*/}
+      <img
+        src="/logo-horizontal.png"
+        alt=""
+        className="h-[28px] w-auto object-contain"
+      />
+      <span className="display whitespace-nowrap text-[17px] lowercase text-white">
+        {site.displayName}
+      </span>
     </div>
   );
 }
